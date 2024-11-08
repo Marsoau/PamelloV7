604595473c54759d3689d7e0876972dce775177c
Marsoau <koteika5063@gmail.com>
2024-11-08 15:45:15 +0200

    Add project files.

---
+++ PamelloV7.Client/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PamelloV7.Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e) {
+
+        }
+    }
+}
---
+++ PamelloV7.Server/Program.cs
@@ -0,0 +1,20 @@
+namespace PamelloV7.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
