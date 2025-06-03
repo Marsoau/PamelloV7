7a2949d835f781fab42051829350e8b71e72a287
Marsoau <koteika5063@gmail.com>
2025-06-03 22:42:45 +0200

    Some events name changes

--- PamelloV7.Core/Enumerators/EEventName.cs
+++ PamelloV7.Core/Enumerators/EEventName.cs
@@ -6,6 +6,78 @@ using System.Threading.Tasks;
 
 namespace PamelloV7.Core.Enumerators
 {
+    public enum EEventNameNew
+    {
+        //EVENTS STATUS UPDATES
+        EventsConnected = 1,
+        EventsAuthorized,
+        EventsUnAuthorized,
+        
+        //SELECTED PLAYER
+        PlayerNameUpdated,
+        PlayerStateUpdated,
+        PlayerIsPausedUpdated,
+        PlayerProtectionUpdated,
+        PlayerCurrentSongIdUpdated,
+        PlayerQueueEntriesDTOsUpdated,
+        PlayerQueuePositionUpdated,
+        PlayerCurrentEpisodePositionUpdated,
+        PlayerNextPositionRequestUpdated,
+        PlayerCurrentSongTimePassedUpdated,
+        PlayerCurrentSongTimeTotalUpdated,
+        PlayerQueueIsRandomUpdated,
+        PlayerQueueIsReversedUpdated,
+        PlayerQueueIsNoLeftoversUpdated,
+        PlayerQueueIsFeedRandomUpdated,
+        
+        //ENTITIES CHANGES
+        //user
+        UsersAvailableUpdated,
+        
+        UserNameUpdated,
+        UserAvatarUpdated,
+        UserSelectedPlayerIdUpdated,
+        UserSongsPlayedUpdated,
+        UserAddedSongsUpdated,
+        UserAddedPlaylistsUpdated,
+        UserFavoriteSongsUpdated,
+        UserFavoritePlaylistsUpdated,
+        UserIsAdministratorUpdated,
+        
+        //song
+        SongsAvailableUpdated,
+        
+        SongNameUpdated,
+        SongCoverUrlUpdated,
+        SongPlayCountUpdated,
+        SongAssociationsUpdated,
+        SongFavoriteByIdsUpdated,
+        SongEpisodesIdsUpdated,
+        SongPlaylistsIdsUpdated,
+        SongDownloadStarted,
+        SongDownloadProgressUpdated,
+        SongDownloadFinished,
+        
+        //episode
+        EpisodesAvailableUpdated,
+        
+        EpisodeNameUpdated,
+        EpisodeStartUpdated,
+        EpisodeSkipUpdated,
+        
+        //playlist
+        PlaylistsAvailableUpdated,
+        
+        PlaylistNameUpdated,
+        PlaylistProtectionUpdated,
+        PlaylistSongsUpdated,
+        PlaylistFavoriteByIdsUpdated,
+        
+        //speaker
+        SpeakersAvailableUpdated,
+        
+        //SERVER MESSAGES
+    }
     public enum EEventName
     {
         EventsConnected,
