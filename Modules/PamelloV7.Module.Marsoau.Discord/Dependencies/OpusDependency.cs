5c7d0172b9f3ef2c65c826860659815d6c8721c8
Marsoau <koteika5063@gmail.com>
2026-03-12 21:21:25 +0200

    Change lib dependencies names back to .so because of windows

--- Modules/PamelloV7.Module.Marsoau.Discord/Dependencies/OpusDependency.cs
+++ Modules/PamelloV7.Module.Marsoau.Discord/Dependencies/OpusDependency.cs
@@ -7,7 +7,7 @@ namespace PamelloV7.Module.Marsoau.Discord.Dependencies;
 public class OpusDependency : MarsoauLibDependency
 {
     public override string Name => "opus";
-    public override string InternalFilePath => "libopus";
+    public override string InternalFilePath => "libopus.so";
     protected override string[] DllNames => ["opus", "libopus"];
 
     public override Assembly DllAssembly => typeof(DiscordSocketClient).Assembly;
--- Modules/PamelloV7.Module.Marsoau.Discord/Dependencies/SodiumDependency.cs
+++ Modules/PamelloV7.Module.Marsoau.Discord/Dependencies/SodiumDependency.cs
@@ -7,7 +7,7 @@ namespace PamelloV7.Module.Marsoau.Discord.Dependencies;
 public class SodiumDependency : MarsoauLibDependency
 {
     public override string Name => "sodium";
-    public override string InternalFilePath => "libsodium";
+    public override string InternalFilePath => "libsodium.so";
     protected override string[] DllNames => ["sodium", "libsodium"];
 
     public override Assembly DllAssembly => typeof(DiscordSocketClient).Assembly;
