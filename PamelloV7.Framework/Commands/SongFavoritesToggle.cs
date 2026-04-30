e591b7fbea4b98bcbdde15a4df0c22fbf9b3b84d
Marsoau <koteika5063@gmail.com>
2026-04-30 20:40:12 +0300

    Song favorite toggle command

---
+++ PamelloV7.Framework/Commands/SongFavoritesToggle.cs
@@ -0,0 +1,20 @@
+using PamelloV7.Framework.Attributes;
+using PamelloV7.Framework.Commands.Base;
+using PamelloV7.Framework.Entities;
+using PamelloV7.Framework.Logging;
+
+namespace PamelloV7.Framework.Commands;
+
+[PamelloCommand]
+public partial class SongFavoritesToggle
+{
+    public void Execute(IEnumerable<IPamelloSong> songs) {
+        var songsList = songs.ToList();
+
+        var songsToAdd = songsList.Where(song => !song.FavoriteBy.Contains(ScopeUser));
+        var songsToRemove = songsList.Where(song => song.FavoriteBy.Contains(ScopeUser));
+        
+        ScopeUser.AddFavoriteSongs(songsToAdd);
+        ScopeUser.RemoveFavoriteSongs(songsToRemove);
+    }
+}
